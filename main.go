361256b631e6dcc3bb6c232acdcf155c9df7c702
WLAdemon <wladoseid@gmail.com>
2021-06-26 20:09:21 +0300

    simple file Cache on Go | part 2

--- main.go
+++ main.go
@@ -7,23 +7,15 @@ import (
 )
 
 func main() {
-
 	cache.InitCache("cache")
-	// err := cache.CreateForever("TEST", "GErttrrrfffdhjавываыаыаываыj").Set()
-	// if err != nil {
-	// 	return
-	// }
 	exist, value := cache.Get("TEST")
 	if exist {
 		fmt.Println(value)
 	} else {
 		fmt.Println("Error")
 	}
-
-	// fileWrite := file.File{Name: "cache/init.set"}
-	// _, err := fileWrite.WriteNewLine("123")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fileWrite.Clear()
+	err := cache.CreateForever("TEST", "Hello world!").Set()
+	if err != nil {
+		return
+	}
 }
